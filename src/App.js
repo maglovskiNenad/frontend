bbf02a3e0101d4dab2f31255e20c534b24483992
maglovskiNenad <maglovskin@gmail.com>
2023-09-25 21:37:12 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+
+import MainPage from "./components/home/MainPage";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/home/MainPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainPage = () => {
+  return <p>Radi</p>;
+};
+
+export default MainPage;
