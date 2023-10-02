0d8ffbc0e3c4301fd09e946bbe3320be9b07048b
maglovskiNenad <maglovskin@gmail.com>
2023-10-02 22:10:03 +0200

    theme

---
+++ src/UI/them.js
@@ -0,0 +1,19 @@
+const theme = {
+  initialColorMode: "light",
+  colors: {
+    text: "black",
+    background: "white",
+    primary: "#282c34",
+    inverseText: "white",
+    modes: {
+      dark: {
+        text: "white",
+        background: "#0f172a",
+        primary: "#282c34",
+        inverseText: "green",
+      },
+    },
+  },
+};
+
+export default theme;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { ThemeUIProvider } from "theme-ui";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import theme from "./UI/them";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeUIProvider theme={theme}>
+      <App />
+    </ThemeUIProvider>
   </React.StrictMode>
 );
 
 reportWebVitals();
+
+//Themaje postavljena neophodno je da se napravi funkcije i da se postavi u nav bar sledeci korak je napraviti nav bar
