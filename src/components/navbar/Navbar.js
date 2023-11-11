8a1dfa65bdbe77492a043a70b693e95fe1d9c40b
maglovskiNenad <maglovskin@gmail.com>
2023-11-11 18:57:00 +0100

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, useColorMode } from "theme-ui";
 import NavComponenst from "./NavComonents";
 import { BiSolidMoon, BiSolidSun } from "react-icons/bi";
@@ -8,11 +8,31 @@ import "./Navbar.css";
 const Navbar = () => {
   const [colorMode, setColorMode] = useColorMode();
   const [state, setState] = useState();
+  //reaguje na sve ekrane podesi da REAGUJE SAMO NA MALE EKRANE,dodaj animaciju
+  const [showMenu, setShowMenu] = useState();
+
+  const handlingDisplaySize = () => {
+    //ovom funkcijom dobijas dimenziju ekrana pokusaj da na to odreaguje nav bar
+    let width = window.innerWidth;
+    console.log(width);
+  };
 
   const handleThemeChange = () => {
     setColorMode(colorMode === "light" ? "dark" : "light");
     setState(colorMode === "light" ? "dark" : "light");
   };
+
+  useEffect(() => {
+    setColorMode(colorMode === "light" ? "light" : "dark");
+    setState(colorMode === "light" ? "light" : "dark");
+  }, []);
+
+  const handleNavMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  handlingDisplaySize();
+
   return (
     <div className="navbar-wraper">
       <NavComponenst />
