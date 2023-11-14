0cf29ec5f2eae16d8ddd02845c511d10f91ac326
maglovskiNenad <maglovskin@gmail.com>
2023-11-14 23:19:55 +0100

    text

--- src/DOC/HTML/HTMLData.js
+++ src/DOC/HTML/HTMLData.js
@@ -1,6 +1,14 @@
 export const HTML_DATA = [
   {
+    description: "<!DOCTYPE>",
+    text: "The declaration is not an HTML tag. It is an information to the browser about what document type to expect.",
+  },
+  {
     description: "HTML <a> Tag",
     text: "An HTML tag is a piece of markup language used to indicate the beginning and end of an HTML element in an HTML document. As part of an HTML element, HTML tags help web browsers convert HTML documents into web pages.",
+  },
+  {
+    description: "<acronym>",
+    text: "The <acronym> element and title attribute was used to associate a full-text explanation with an acronym. The <acronym> element has been deprecated in HTML5 and <abbr> should be used instead. ",
   },
 ];
--- src/components/navbar/NavComonents.js
+++ src/components/navbar/NavComonents.js
@@ -5,6 +5,12 @@ const NavComponenst = () => {
     <div className="nav-fragment">
       <div className="nav">
         <ul className="u-list">
+          <li className="burger-menu">
+            <span className="one"></span>
+            <span className="one two"></span>
+            <span className="one"></span>
+          </li>
+
           <li className="element">Home</li>
           <li className="element">About</li>
           <li className="element">Components</li>
