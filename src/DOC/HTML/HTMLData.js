a118e07d3ff0196cb4e490d606e088a01af11009
maglovskiNenad <maglovskin@gmail.com>
2023-11-19 14:46:11 +0100

    text

--- src/DOC/HTML/HTMLData.js
+++ src/DOC/HTML/HTMLData.js
@@ -3,15 +3,18 @@ export const HTML_DATA = [
     description: "<!DOCTYPE>",
     text: "The <!DOCTYPE> declaration is an instruction used in HTML (Hypertext Markup Language) documents to specify the document type and version of HTML being used. It stands for 'Document Type Definition.' The <!DOCTYPE> declaration is placed at the very beginning of an HTML document, before the <html> tag.The purpose of the <!DOCTYPE> declaration is to inform the web browser about the version of HTML in which the document is written. It helps the browser to render the page correctly by following the specified rules and standards associated with that HTML version. The <!DOCTYPE> declaration is not an HTML tag; rather, it is a directive to the browser.",
     id: 1,
+    example: "",
   },
   {
     description: "HTML <a> Tag",
-    text: "An HTML tag is a piece of markup language used to indicate the beginning and end of an HTML element in an HTML document. As part of an HTML element, HTML tags help web browsers convert HTML documents into web pages.",
+    text: "The <a> tag in HTML is used to create hyperlinks, allowing you to link to other web pages, resources, or files. The 'a' in <a> stands for 'anchor', reflecting its historical use for creating anchor points within a document. However, its primary purpose in modern web development is for creating hyperlinks.",
     id: 2,
+    example: "",
   },
   {
     description: "<acronym>",
-    text: "The <acronym> element and title attribute was used to associate a full-text explanation with an acronym. The <acronym> element has been deprecated in HTML5 and <abbr> should be used instead. ",
+    text: "The <acronym> element was part of earlier versions of HTML and was used to define an acronym. An acronym is a word formed from the initial letters of other words, such as 'NASA' (National Aeronautics and Space Administration) or 'HTML' (Hypertext Markup Language). The <acronym> element provided a way to mark up and explain acronyms in HTML documents. ",
     id: 3,
+    example: "",
   },
 ];
