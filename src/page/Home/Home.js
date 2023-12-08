a9a9c813d670f58bb3c84bc6289c6b580e0072e6
maglovskiNenad <maglovskin@gmail.com>
2023-12-08 18:48:19 +0100

    home page

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,7 +1,39 @@
 import "./Home.css";
 
 const Home = () => {
-  return <p>Home page</p>;
+  return (
+    <div className="home-page-wraper">
+      <div className="home-title">
+        <h2 className="title">Getting Started with Frontend Career</h2>
+        <p className="home-text">
+          The purpose of this roadmap is to give you an idea about the frontend
+          development. The road map will guide you if you are confused about
+          what to learn next, rather than encouraging you to pick what is hype
+          and trendy. You should grow some understanding of why one tool would
+          be better suited for some cases than the other and remember that hype
+          and trendy do not always mean best suited for the job.
+        </p>
+        <p className="home-text">
+          What you will be able to see as a scenario is a text page about the
+          possibilities and assumptions that can be used when learning and
+          mastering the skills of a frontend developer. At the same time, all
+          this is a small idea and a path towards realization for all of us who
+          always try to do our best in order to become better and more
+          successful in overcoming obstacles and solving daily problems. The
+          following information that you can find here is of a text type, you
+          could say a mini library of information that can be used for your
+          project as well.
+        </p>
+        <p className="home-text">
+          Of course, this site is not a site that will explain to you how to
+          solve some of the problems you have encountered on your way to a
+          front-end development career, but it is certainly a small treasure of
+          valuable information that you can certainly remind yourself of how
+          some of the elements in the world of programming should you use.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
