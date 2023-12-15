c5a0d8dd9e8839d59057b70792f2351f7f614eba
maglovskiNenad <maglovskin@gmail.com>
2023-12-15 21:21:39 +0100

    window functionality

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavbarList from "./NavbarComponents/NavbarList";
 import NavbarBurgerMenu from "./NavbarComponents/NavbarBurgerMenu";
 import NavbarBtnTheme from "./NavbarComponents/NavbarBtnTheme";
@@ -7,21 +7,33 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const [burgerMenuIsVisible, setBurgerMenuIsVisible] = useState(false);
+  // Velicina ekrana
+  const [windowSize, setWindowSize] = useState({ width: window.innerWidth });
 
-  // const [windowSize, setWindowSize] = useState({ width: window.innerWidth });
-  // const handleResize = () => {
-  //   setWindowSize({
-  //     width: window.innerWidth,
-  //   });
-  // };
-  // useEffect(() => {
-  //   window.addEventListener("resize", handleResize);
-  //   return () => {
-  //     window.addEventListener("resize", handleResize);
-  //   };
-  // }, []);
+  // Ugnjezdijivanje velicine u funkciju
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+    });
+  };
 
+  // Funkcija koja se stalno poziva na minimalno promeni sirine ekrana
+  useEffect(() => {
+    // Poziv promene ekrana
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.addEventListener("resize", handleResize);
+    };
+  }, []);
+  //Uslov za proveravenje stanja useState koji je postavljen gore
+  if (windowSize.width >= 480 && burgerMenuIsVisible !== true) {
+    // U slucaju da je uslov ispunjen promena stanja
+    setBurgerMenuIsVisible(true);
+  }
+
+  //Promena stanja vidljivosti dropdown menija
   const dropdownMenuHandler = () => {
+    //Svakim klikom se menja stanje
     setBurgerMenuIsVisible(!burgerMenuIsVisible);
   };
 
